b183c8715b57f5be92c18ae00a1a07b1b6997d60
Sudharsanan Velmurugan <106078823+sudharsanan-v@users.noreply.github.com>
2024-07-29 22:22:29 +0530

    Updated Login page css and sign page
    Imported fa command
    npm install react-icons --save

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Home from './Components/Home/Home';
 import Login from './Components/Login/Login';
 import Header from './Components/Navbar/Header';
+import SignIn from './Components/Sign In/SignIn';
 function App() {
   return (
     <div >
      <Home/>
      <Header/>
      <Login/>
+     <SignIn/>
     </div>
   );
 }
--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -3,23 +3,23 @@ import './Login.css'
 import { FaUser, FaLock } from "react-icons/fa";
 const Login = () => {
     return (
-        <div className='wrapper'>
+        <div className='login-page-container'>
             <h1>Login Page</h1>
-            <form action="">
-                <div className="input-box">
-                    <input type="text" placeholder="User Name" required/>
+            <form>
+                <div className="user-name-container">
+                    <input type="text" placeholder="UserName" required/>
                     <FaUser className="icon"/>
                 </div>
-                <div className="input-box">
+                <div className="user-password-container">
                     <input type="password" placeholder="Password" required/>
                     <FaLock className="icon"/>
                 </div>
-                <div className="remember-forgot">
+                <div className="login-checkbox">
                 <label><input type="checkbox" /> Remember me</label>
                     <a href="">forget password</a>
                 </div>
                 <button type="submit">Log In</button>
-                <div className="register-link">
+                <div className="login-register-link">
                     <p>Don't have any account? <a href="#">Sign In</a></p>
                 </div>
 
