0b300d9210bf47ab891c736c0f4ce119f492eb86
Sudharsanan-Grl <121048921+sudharsanan-velmurugan@users.noreply.github.com>
2024-08-05 22:20:25 +0530

    added Link elements from react route

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import './App.css';
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Slideshow from './Background/Slideshow';
 import Login from './Components/Login/Login';
 import Header from './Components/Navbar/Header';
 import SignIn from './Components/Sign In/SignIn';
+import Home from './Components/Home/Home';
+import About from './Components/About/About';
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path='/'  element={ <Slideshow/>}/>
-      <Route path='/header'  element={  <Header/>}/>
-      <Route path='/login'  element={ <Login/>}/>
-      <Route path='/signin'  element={ <SignIn/>}/>
-    </Routes>   
-    </BrowserRouter>     
+      <Header />
+      <Slideshow />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/services' element={<Login />} />
+        <Route path='/contact' element={<Login />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signin' element={<SignIn />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
