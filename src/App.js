12bc9c6abdf48852dc2d1559387741dfa4b087b2
Pavithra <poongothaipavi2001@gmail.com>
2024-08-10 14:08:24 +0530

    Reset Password & Forgot Password  Page created

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import Header from './Components/Navbar/Header';
 import SignIn from './Components/Sign In/SignIn';
 import Home from './Components/Home/Home';
 import About from './Components/About/About';
-import Reset from './Components/Reset Password/Reset';
+import Reset from './Components/Reset/Reset';
+import Forgot from './Components/Forgot/Forgot';
+import Service from './Components/Service/Service'
+import Contact from './Components/Contact/Contact'
 function App() {
   return (
     <BrowserRouter>
@@ -15,11 +18,12 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
-        <Route path='/services' element={<Login />} />
-        <Route path='/contact' element={<Login />} />
-        <Route path='/reset' element={<Reset />} />
+        <Route path='/services' element={<Service />} />
+        <Route path='/contact' element={<Contact />} />
         <Route path='/login' element={<Login />} />
         <Route path='/signin' element={<SignIn />} />
+        <Route path='reset' element={<Reset />} />
+        <Route path='forgot' element={<Forgot />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/Components/Forgot/Forgot.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Forgot.css'
+
+export default function Forgot() {
+  return (
+    <div className='forgot-password-container'>
+        <h1>Forgot Password</h1>
+        <form>
+        <div className='forgot-password'>
+            <label>Email Id</label>
+            <input type='email' placeholder='Enter Your mail id' required/>
+        </div>
+        <button type='submit'>submit</button>
+        </form>
+    </div>
+  )
+}
