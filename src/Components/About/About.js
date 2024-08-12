653e2abb95997e0fa52156cf31a67131b4a77259
Sudharsanan-Grl <121048921+sudharsanan-velmurugan@users.noreply.github.com>
2024-08-12 18:32:18 +0530

    updated home component

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -5,14 +5,9 @@ const About = () => {
   return (
     <div className='about-page-container'>
       <h1>Kaarvens Structural Consultancy</h1>
-      <p>Designing | Consultancy | Checking</p>
-      <p>Coimbatore.</p>
+      <p>Designing | Consultancy | Checking <br />Coimbatore.</p>
       <p>Young dynamic team, with determination to solve most of the challenging architecture.</p>
       <p>Expectations will be exceeded by our service.</p>
-      <form>
-      <button type='submit'>Signin</button>
-      <button type='submit'>Login</button>
-      </form>
     </div>
   )
 }
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import About from '../About/About'
+import { Link } from 'react-router-dom'
+import './Home.css'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <About/>
+     <div className='links-container'>
+     <Link className='nav-elements' to="/signin">Sign in</Link>
+     <Link className='nav-elements' to="/login">Login</Link>
+      </div> 
+     
+    </div>
   )
 }
 
