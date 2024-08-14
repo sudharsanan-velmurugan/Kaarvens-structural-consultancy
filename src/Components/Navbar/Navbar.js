3ad8f226fafb237416b725d6faec831588fcbc91
Sudharsanan-Grl <121048921+sudharsanan-velmurugan@users.noreply.github.com>
2024-08-14 19:56:58 +0530

    routes protected

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import '../Navbar/Navbar.css'
 
 const Navbar = () => {
+  const [login,setLogin]=useState(true)
   return (
     <div className='Nav-Container'>
       <ul>
@@ -10,10 +11,10 @@ const Navbar = () => {
         <li> <Link className='nav-elements' to="/About">About</Link></li>
         <li> <Link className='nav-elements' to="/Services">Services</Link></li>
         <li> <Link className='nav-elements' to="/Contact">Contact</Link></li>
-        <li> <Link className='nav-elements' to="/Forgot">Forgot</Link></li>
+        <li onClick={()=>setLogin(!login)}> <Link className='nav-elements' to="/login">Login</Link> </li>
+        { login &&(<><li> <Link className='nav-elements' to="/Forgot">Forgot</Link></li>
         <li> <Link className='nav-elements' to="/Reset">Reset</Link></li>
-        <li> <Link className='nav-elements' to="/signin">Sign in</Link> </li>
-        <li> <Link className='nav-elements' to="/login">Login</Link> </li>
+        <li> <Link className='nav-elements' to="/signin">Sign in</Link> </li></>)}
       </ul>
 
     </div>
