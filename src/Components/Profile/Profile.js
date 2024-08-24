3b170b1de15e38c14b9ca36ecbffdd3d66e7316c
Pavithra <poongothaipavi2001@gmail.com>
2024-08-24 16:58:49 +0530

    profile page styled

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -3,21 +3,29 @@ import profileImg from '../../Images/profile.jpg'
 import './Profile.css'
 const Profile = () => {
   return (
-    <div>
-       <div className='profile-page-container'>
+    <div className='profile-page-container'>
+       <h1>Profile</h1> 
+        <div className='profile-page-image'>
             <img src={profileImg} alt="Profile img" />
-            <label>First Name</label>
-            <input type='text'></input>
-            <label>Last Name</label>
-            <input type='text'></input>
-            <label>Email</label>
-            <input type='email'></input>
-            <label>Mobile No</label>
-            <input type='tel'></input>
-            <p>Change Password</p>
-            <button type='Submit'>Logout</button>
-
         </div>
+        <div className='profile-page-fname'>
+            <label>First Name :</label>
+            <input type='text'></input>
+        </div>
+        <div className='profile-page-lname'>
+            <label>Last Name :</label>
+            <input type='text'></input>
+        </div>
+        <div className='profile-page-email'>
+            <label>Email Id :   </label>
+            <input type='email'></input>
+        </div>
+        <div className='profile-page-mobileno'>
+          <label>Mobile No :</label>
+          <input type='tel'></input>
+        </div>
+        <p>Change Password</p>
+        <button type='submit'>Logout</button>
     </div>
   )
 }
