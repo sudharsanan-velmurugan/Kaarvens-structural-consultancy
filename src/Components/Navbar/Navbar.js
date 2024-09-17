6ceefe593ebcd95133d6cbb7f71d964bc4d66c9a
Pavithra <poongothaipavi2001@gmail.com>
2024-09-17 18:29:17 +0530

    icon changed

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -2,8 +2,8 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import '../Navbar/Navbar.css'
 import { useSelector } from 'react-redux'
-import { FaHome, FaEdit, FaUsers, FaUser } from "react-icons/fa";
-import { GrProjects } from "react-icons/gr";
+import { FaHome, FaEdit, FaUsers, FaUser, FaThList } from "react-icons/fa";
+
 
 const Navbar = () => {
   
@@ -14,7 +14,7 @@ const Navbar = () => {
       <ul>
         <li><Link className='nav-elements' to="/"><FaHome /></Link></li>
         <li><Link className='nav-elements' to="/about"><FaEdit /></Link></li>
-        <li><Link className='nav-elements' to="/projects"><GrProjects /></Link></li>
+        <li><Link className='nav-elements' to="/projects"><FaThList /></Link></li>
         {isAdminLogin && (
           <li><Link className='nav-elements' to="/users"><FaUsers /></Link></li>
         )}
